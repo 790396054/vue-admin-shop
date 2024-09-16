9a20ec0a4c91acd699363c825603cb2ffb980f40
gmm <790396054@qq.com>
2024-09-16 20:43:20 +0800

    增加组件缓存

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -9,7 +9,11 @@
         </el-aside>
         <el-main>
             <f-tag-list></f-tag-list>
-            <router-view></router-view>
+            <router-view v-slot="{ Component }">
+              <keep-alive :max="10">
+                <component :is="Component"></component>
+              </keep-alive>
+            </router-view>
 
             <div class="text-gray-400 text-xs font-thin" style="text-align: center;margin: 35px 0;height: 50px">本站点由 <a href="https://study.163.com/course/courseMain.htm?courseId=1212775807&share=2&shareId=480000001892585" target="_blank" class="font-normal border-b border-purple-300 text-purple-400">《Vue3实战商城后台管理系统开发视频课程》</a> 提供</div>
         </el-main>
